e8396e519c6366b2a30e5f14cdc60ec5687637af
Emanuel Rusu <emanuelrusu91@example.com>
2025-06-02 23:40:58 +0300

    Create sitemap index following Google documentation

--- next.config.mjs
+++ next.config.mjs
@@ -9,14 +9,6 @@ const nextConfig = {
   images: {
     domains: ["book-reviews-git-main-webira-rem-srls-projects.vercel.app"],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/sitemap.xml",
-        destination: "/minimal-sitemap.xml",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
